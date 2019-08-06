e1bd7d662ccebb2c3edbaa6b6180069aaa696074
unknown <justinchoi824@gmail.com>
2019-08-05 20:01:37 -0700

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+console.log('Client side javascript file is loaded');
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    messageOne.textContent = 'Loading!';
+    messageTwo.textContent = '';
+
+    const location = 'http://localhost:3000/weather?address=' + encodeURIComponent(search.value);
+    fetch(location).then((response) => {
+        response.json().then((data) => {
+            if (!data.error){
+                const datas = data.forecastData;
+                messageOne.textContent = data.location;
+                messageTwo.textContent = datas.precipitation + " " + datas.summary + ". " + datas.temperature;
+            }
+            else{
+                messageOne.textContent = data.error;
+            }
+        });
+    });
+
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = "https://api.darksky.net/forecast/83356f9eb05aa31b686575469ad11959/" + encodeURIComponent(latitude) +  "," + encodeURIComponent(longitude);
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!');
+        }
+        else if (body.error){
+            callback('Location is not found. Try another search', undefined);
+        }
+        else{
+            callback(undefined, {
+                summary: body.currently.summary,
+                temperature: "The temperature is " + body.currently.temperature + " degrees out.",
+                precipitation: "There is a " + body.currently.precipProbability + "% chance of rain."
+            });
+            // callback(undefined, response.body.daily.data[0].summary + '. It is currently ' +  response.body.currently.temperature + ' degrees out. There is a '+ response.body.currently.precipProbability + "% chance of rain.")
+        }
+    });
+};
+
+module.exports = forecast;
